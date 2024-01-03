c7733a8781767f34f3e2e9f98aaec7137a3dd2e9
dipeshdeula <deuladipesh94@gmail.com>
2024-01-03 20:16:34 +0545

    Tower of Hanoi updated

--- Lab1.c
+++ Lab1.c
@@ -1,6 +1,4 @@
 /* Implementation of different operations related to stack
-
-
 #include<stdio.h>
 #define MAXSIZE 5
 void PUSH();
@@ -167,3 +165,23 @@ int main(){
 }
 
 */
+
+//Implement the solution of TOH and Fibonacci Series using Recursion
+
+//Tower of Hanoi
+
+#include<stdio.h>
+void towerOfHanoi(int n,char source,char auxiliary,char destination){
+    if(n==1){
+        printf("Move disk 1 from %c to %c\n",source,destination);
+        return;
+    }
+    towerOfHanoi(n-1,source,destination,auxiliary);
+    printf("Move disk %d from %c to %c\n",n,source,destination);
+    towerOfHanoi(n-1,auxiliary,source,destination);
+}
+int main(){
+    int n=3;
+    towerOfHanoi(n,'A','B','C');
+    return 0;
+}
