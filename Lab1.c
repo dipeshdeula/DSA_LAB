037eaf56f1fd8c829fd8449556afa4f12a101a8b
dipeshdeula <deuladipesh94@gmail.com>
2024-01-03 15:32:06 +0545

    Updated

--- Lab1.c
+++ Lab1.c
@@ -18,5 +18,55 @@ int main(){
     PUSH(20);
     PUSH(10);
     PUSH(5);
+    display();
+    search(10);
+    search(5);
+    POP();
+    display();
 }
-
+void PUSH(int a){
+    if(isFull()){
+        printf("Stack Overflow\n");
+    }else
+    {
+       top=top+1;
+       arr[top]=a;
+       printf("%d pushed into the Stack\n",a);
+    }
+    
+}
+void display(){
+    if(isEmpty()){
+        printf("Stack Underflow \n");
+    }else{
+        for(int i=top;i>=0;i--){
+            printf("\n%d\t ",arr[i]);
+        }
+    }
+}
+void POP(){
+    if(isEmpty()){
+        printf("Stack Underflow\n");
+    }else{
+        printf("\n%d Poped from the Stack\n",arr[top]);
+        top=top-1;
+    }
+}
+void search(int x){
+    int i;
+    for(i=0;i<=top;i++){
+        if(arr[i]==x){
+            printf("\nElement found at position %d\n",x);
+            break;
+        }
+    }
+    if(i==top+1){
+        printf("\n%dElement not Found in the Stack\n",x);
+    }
+}
+int isFull(){
+    return (top == MAXSIZE - 1) ? 1 : 0;
+}
+int isEmpty(){
+    return (top == -1) ? 1 : 0;
+}
